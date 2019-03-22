6d3cc6e1263ade34e70318bc69691527b79113a7
baiyunsong <baiyunsong@kingsoft.com>
2019-03-22 16:08:59 +0800

    动态路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,23 @@
 import Vue from "vue";
 import Router from "vue-router";
-import homeConfig from "./home";
-import aboutConfig from "./about";
-
+/** -------------以下为手动注册路由的代码------------- */
+// import homeConfig from "./home";
+// import aboutConfig from "./about";
+// const routes = [...homeConfig, ...aboutConfig];
+/** -------------以上为手动注册路由的代码------------- */
 Vue.use(Router);
 
-const routes = [...homeConfig, ...aboutConfig];
-
+/** -------------以下为自动注册路由的代码------------- */ 
+let routes = [];
+const routerContext = require.context('.', true, /index\.js/)
+routerContext.keys().forEach(routeName => {
+  if (routeName.startsWith("./index")) return // 排除根index.js
+  const routeModule = routerContext(routeName) // get router module object
+  routes = [...routes, ...(routeModule.default || routeModule)] // 注册路由
+});
+/** -------------以上为自动注册路由的代码------------- */
 export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: routes
-  // routes: [
-  //   {
-  //     path: '/',
-  //     name: 'home',
-  //     component: Home
-  //   }
-  // ]
 });
