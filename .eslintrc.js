f30d86179d7688b4964d6b11e0216309d81c45f8
baiyunsong <baiyunsong@kingsoft.com>
2019-03-22 14:44:10 +0800

    动态注册全局组件配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,11 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     "quotes": [0], // 引号类型 `` "" ''
+    "semi": 'off',
+    "space-before-function-paren": "off",
+    "no-trailing-spaces": "off",
+    "eol-last": "off",
+    "no-multi-spaces": 'off',
     "no-extra-semi": "error" /** 忽略额外的分号 */
   },
   parserOptions: {
--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router/router.js'
-import store from './store/store.js'
-import i18n from './i18nConfig/i18nConfig.js'
-Vue.config.productionTip = false
-window.Vue = Vue
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router/router.js";
+import store from "./store/store.js";
+import i18n from "./i18nConfig/i18nConfig.js";
+import "@/components/global" /** 辅助引用代码 */
+
+Vue.config.productionTip = false;
+window.Vue = Vue;
 new Vue({
   router,
   store,
   i18n,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
