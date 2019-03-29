193a88d79d89ddd3226b645f54bdfb60e4726a9b
baiyunsong <baiyunsong@kingsoft.com>
2019-03-29 16:20:19 +0800

    删除多余的代码

--- src/components/index.js
+++ src/components/index.js
@@ -3,9 +3,6 @@ import Vue from "vue";
 
 // 自动加载 global 目录下的 .js 结尾的文件 https://webpack.js.org/guides/dependency-management/#requirecontext
 const componentsContext = require.context("./global", true, /\.js$/);
-
-console.log(66666, componentsContext)
-
 componentsContext.keys().forEach(component => {
   const componentConfig = componentsContext(component);
   /**
@@ -14,39 +11,3 @@ componentsContext.keys().forEach(component => {
   const ctrl = componentConfig.default || componentConfig;
   Vue.component(ctrl.name, ctrl);
 });
-
-
-
-// import upperFirst from 'lodash/upperFirst'
-// import camelCase from 'lodash/camelCase'
-
-// const requireComponent = require.context(
-//   // 其组件目录的相对路径
-//   './components',
-//   // 是否查询其子目录
-//   false,
-//   // 匹配基础组件文件名的正则表达式
-//   /Base[A-Z]\w+\.(vue|js)$/
-// )
-
-// requireComponent.keys().forEach(fileName => {
-//   // 获取组件配置
-//   const componentConfig = requireComponent(fileName)
-
-//   // 获取组件的 PascalCase 命名
-//   const componentName = upperFirst(
-//     camelCase(
-//       // 剥去文件名开头的 `./` 和结尾的扩展名
-//       fileName.replace(/^\.\/(.*)\.\w+$/, '$1')
-//     )
-//   )
-
-//   // 全局注册组件
-//   Vue.component(
-//     componentName,
-//     // 如果这个组件选项是通过 `export default` 导出的，
-//     // 那么就会优先使用 `.default`，
-//     // 否则回退到使用模块的根。
-//     componentConfig.default || componentConfig
-//   )
-// })
