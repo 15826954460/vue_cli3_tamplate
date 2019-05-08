3789f0d14f5989804884ab99174fa761a2ef3b63
baiyunsong <baiyunsong@wps.cn>
2019-05-08 15:03:29 +0800

    [新建分支重写vue.elm]

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import router from "./router/index.js";
 import store from "./store/store.js";
 import i18n from "./i18nConfig/i18nConfig.js";
 import "@/components/index.js" /** 直接引用js */
-// import '@/mixin/mixin.js' // 全局混入的用法，对应 mixin/mixin.js 中注释代码
 Vue.config.productionTip = false;
 new Vue({
   router,
--- src/mixin/mixin.js
+++ src/mixin/mixin.js
@@ -16,18 +16,3 @@ export default {
     ...mapActionsHome(["at_add"])
   }
 }
-
-// 全局混入，不建议使用，会增加不必要的代码
-// Vue.mixin({
-//   created() {
-//     console.log(6666666666)
-//   },
-//   computed: {
-//     ...mapState(["userInfo"]),
-//     ...mapStateHome(["count"])
-//   },
-//   methods: {
-//     ...mapActions(["ac_setUserInfo"]),
-//     ...mapActionsHome(["at_add"])
-//   }
-// })
