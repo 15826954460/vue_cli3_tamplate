f90c90d67a36f501c5074ad343f9739dbafc8e2e
baiyunsong <624793604@qq.com>
2019-06-10 19:26:46 +0800

    [update]axios封装优化

--- src/axios/apiConfig.js
+++ src/axios/apiConfig.js
@@ -1,8 +1,24 @@
 import axios from "axios";
 
+const TIME_OUT = 50000; // 请求超时时间
+const LOADING_START = 600; // loading开始时间
+const LOADING_END = 30000; // 动画结束时间
+
+let requestInstanceStack = new Map(); // 请球拦截栈
+let responseInstanceStack = new Map(); // 响应拦截栈
+let cancelFetch = new Map(); // 取消请求的拦截栈
+
+
 let customAxios = axios.create({
-  baseURL: "/api/", // 根据实际情况自行配制
-  timeout: 60000, // 默认请求超时时间
+  /**
+   * 开发环境：/android-task/ 本地代理到测试环境
+   * 正式环境：https://micro.api.wps.com/android-task/
+   */
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "/android-task/"
+      : "https://micro.api.wps.com/android-task/",
+  timeout: TIME_OUT, // 默认请求超时时间
   // 设置请求头格式：用自定义的覆盖 axios 自带的 'Content-Type': 'application/x-www-form-urlencoded'
   headers: {
     "Content-Type": "application/json; charset=UTF-8",
@@ -17,123 +33,83 @@ let customAxios = axios.create({
   responseType: "json" // 默认的相应数据格式
 });
 
-const webApiConfig = {
-  startToLoading: 600, // 600ms内网络请求无响应，则展现loading动画
-  loadingTimeout: 30000, // loading 动画超时时间
-  requestInstanceStack: new Map(), // 请球拦截
-  responseInstanceStack: new Map(), // 响应拦截
-  instance: customAxios,  /**  自定义一个 axios 实例 */
-  /**  post 传参序列化  (添加请求拦截器) */
-  setRequestInterceptors: interfaceKey => {
-    let _requestInstance = webApiConfig.instance.interceptors.request.use(
-      config => {
-        /** 根据实际业务写逻辑,比如参数同意进行过滤 */
-        return config;
-      },
-      error => {
-        return Promise.reject(error);
-      }
-    );
-    /** 将请求拦截放到拦截栈中 */
-    webApiConfig.requestInstanceStack.set(interfaceKey, _requestInstance);
-  },
-  /**  移除请求拦截器 */
-  removeRequestInterceptors: interfaceKey => {
-    webApiConfig.instance.interceptors.request.eject(
-      webApiConfig.requestInstanceStack.get(interfaceKey)
-    );
-  },
-  // 设置响应拦截
-  setResponseInterceptors: interfaceKey => {
-    /** 返回状态判断  (添加响应拦截器) */
-    let _responseInstance = webApiConfig.instance.interceptors.response.use(
-      /** 请求成功的回掉 */
-      res => {
-        // 根据http请的响应状态码做一些相应的处理，业务逻辑自己写，这里只是简单的console
-        let { status } = res;
-        switch (status) {
-          case 404:
-            console.log("请求路径有误");
-            break;
-          case 200:
-          case 304:
-            // 针对重定向和请求ok就返回数据
-            return res;
-          case 400:
-            console.log("请求参数有误，请检查");
-            break;
-        }
-      },
-      /** 请求失败的回掉 */
-      error => {
-        return Promise.reject(error);
-      }
-    );
-    /** 将响应拦截放到拦截栈中 */
-    webApiConfig.responseInstanceStack.set(interfaceKey, _responseInstance);
-  },
-  /**  移除响应拦截器 */
-  removeResponseInterceptors: interfaceKey => {
-    webApiConfig.instance.interceptors.response.eject(
-      webApiConfig.responseInstanceStack.get(interfaceKey)
-    );
-  }
-};
-
-/** 开始请求接口，判断是否展示loading动画 */
-function startLoading() {
-  // let _loadInstance = null;
-  // // 600ms之后展示loading动画
-  // let _startTimer = setTimeout(() => {
-  //   _loadInstance = bouncedUtils.loading.show; // 封装的loading动画
-  //   _loadInstance();
-  //   _startTimer = null;
-  // }, webApiConfig.startToLoading);
-
-  // let _overTimer = setTimeout(() => {
-  //   bouncedUtils.toast.show({ content: "请求超时\n请检查网络" }); // bouncedUtils
-  //   _overTimer = null;
-  // }, webApiConfig.loadingTimeout);
-
-  // return [_startTimer, _overTimer, _loadInstance];
+/**  post 传参序列化  (添加请求拦截器) */
+function setRequestInterceptors(interfaceKey) {
+  let _requestInstance = customAxios.interceptors.request.use(
+    config => {
+      /** 根据实际业务写逻辑 */
+      return config;
+    },
+    error => {
+      return Promise.reject(error);
+    }
+  );
+  /** 将请求拦截放到拦截栈中 */
+  requestInstanceStack.set(interfaceKey, _requestInstance);
+}
+/**  移除请求拦截器 */
+function removeRequestInterceptors(interfaceKey) {
+  customAxios.interceptors.request.eject(
+    requestInstanceStack.get(interfaceKey)
+  );
+}
+// 设置响应拦截
+function setResponseInterceptors(interfaceKey) {
+  /** 返回状态判断  (添加响应拦截器) */
+  let _responseInstance = customAxios.interceptors.response.use(
+    res => {
+      /** 根据实际业务写逻辑 */
+      return res;
+    },
+    error => {
+      return Promise.reject(error);
+    }
+  );
+  /** 将响应拦截放到拦截栈中 */
+  responseInstanceStack.set(interfaceKey, _responseInstance);
+}
+/**  移除响应拦截器 */
+function removeResponseInterceptors(interfaceKey) {
+  customAxios.interceptors.response.eject(
+    responseInstanceStack.get(interfaceKey)
+  );
 }
 
-/** 请求接口结束，关闭loading动画 */
+/** 开始请求接口 */
+function startLoading() {
+  let _loadInstance = null;
+  let _startTimer = setTimeout(() => {
+    // 弹出loading框，方法待写
+    _startTimer = null;
+  }, LOADING_START);
+  let _overTimer = setTimeout(() => {
+    // 超时后的弹框内容，待定
+    _overTimer = null;
+  }, LOADING_END);
+  return [_startTimer, _overTimer, _loadInstance];
+}
+
+/** 请求接口结束 */
 function endLoading() {
-  // clearTimeout(startLoading()[0]);
-  // clearTimeout(startLoading()[1]);
-  // startLoading()[2] instanceof Function && bouncedUtils.loading.hide();
+  clearTimeout(startLoading()[0]);
+  clearTimeout(startLoading()[1]);
 }
 
 /** 启用拦截 */
 function startInterceptors(interfaceKey) {
-  webApiConfig.setRequestInterceptors(interfaceKey);
-  webApiConfig.setResponseInterceptors(interfaceKey);
+  setRequestInterceptors(interfaceKey);
+  setResponseInterceptors(interfaceKey);
 }
 
 /** 删除拦截和改拦截实例 */
 function deleteInterceptors(interfaceKey) {
-  webApiConfig.requestInstanceStack.delete(interfaceKey);
-  webApiConfig.responseInstanceStack.delete(interfaceKey);
-  webApiConfig.removeRequestInterceptors(interfaceKey);
-  webApiConfig.removeResponseInterceptors(interfaceKey);
-}
-
-/** 关于取消请求的相关方法 */
-function cancelFetch(cancel, interfaceKey) {
-  /** 保存取消请求的实例对象 */
-  let _cancelObj = {};
-  if (!_cancelObj.cancel) {
-    _cancelObj = {
-      key: interfaceKey,
-      cancel: null
-    };
-  } else if (_cancelObj.cancel) {
-    /** 取消请求,并重置数据 */
-    _cancelObj.cancel();
-    _cancelObj = {};
-  }
-  return _cancelObj;
+  // 清除拦截栈
+  requestInstanceStack.delete(interfaceKey);
+  responseInstanceStack.delete(interfaceKey);
+  cancelFetch.delete(interfaceKey);
+  // 移除拦截
+  removeRequestInterceptors(interfaceKey);
+  removeResponseInterceptors(interfaceKey);
 }
 
 // 对 get 请求简易封装
@@ -141,40 +117,39 @@ export function getFetch({
   url = "",
   params = {},
   interfaceKey = "",
-  cancel = false
+  cancel = false, // 判断是否中断请求的参数
 } = {}) {
-  !cancel && startInterceptors(interfaceKey); // 开启请求拦截
-  if (cancelFetch(cancel, interfaceKey).cancel) return;
+  if (cancel) {
+    cancelFetch.get(interfaceKey)();
+    deleteInterceptors(interfaceKey); // 删除拦截器以及其实例
+    return
+  }
+  startInterceptors(interfaceKey); // 请求拦截
   /** 这里使用 promise 进行就建议包装是为了更友好的将数据的处理暴露在业务层 */
   return new Promise((resolve, reject) => {
-    startLoading();
-    webApiConfig
-      .instance({
-        method: "get",
-        url: url,
-        params: params,
-        cancelToken:
-          (cancel &&
-            webApiConfig.instance.CancelToken(function executor(c) {
-              // executor 函数接收一个 cancel 函数作为参数
-              cancelFetch(cancel, interfaceKey).cancel = c;
-            })) ||
-          ""
-      })
+    startLoading(); // 展示loading
+    customAxios({
+      method: "get",
+      url: url,
+      params: params,
+      cancelToken:
+        (!cancel && customAxios.CancelToken(function executor(c) {
+          // executor 函数接收一个 cancel 函数作为参数
+          cancelFetch.set(interfaceKey, c)
+        })) || ""
+    })
       .then(response => {
-        endLoading();
+        endLoading(); // 隐藏loading
         deleteInterceptors(interfaceKey); // 删除拦截器以及其实例
-        let _code = response.code;
-        /** 根据后台返回的状态码进行相应的处理 */
-        switch (_code) {
-          case 0:
-          case 1:
-          case 200:
-            break;
-          default:
-            console.log("示例代码都进行返回数据");
+        if (response.status === 200) {
+          /** 这里也可以通过制定的成功的毁掉函数来返回数据 */
+          return resolve(response.data);
+        } else {
+          /**
+           * 这里的数据处理请根据实际业务来操作
+           * 比如指定跳转到某页面
+           */
         }
-        resolve(response.data);
       })
       .catch(error => {
         console.log(`请求当前的接口为 ${url} 错误信息为 ${error}`);
@@ -194,38 +169,36 @@ export function postFetch({
   interfaceKey = "",
   cancel = false
 } = {}) {
-  !cancel && startInterceptors(interfaceKey); // 开启请求拦截
+  if (cancel) {
+    deleteInterceptors(interfaceKey); // 删除拦截器以及其实例
+    return cancelFetch.get(interfaceKey)();
+  }
+  startInterceptors(interfaceKey); // 开启请求拦截
   /** 针对可以取消请求的操作做一些响应的处理 */
-  if (cancelFetch(cancel, interfaceKey).cancel) return;
+  if (cancelFetch(interfaceKey).cancel) return;
   /** 这里使用 promise 进行就建议包装是为了更友好的将数据的处理暴露在业务层 */
   return new Promise((resolve, reject) => {
     /** 配置请求是否加载动画 */
-    startLoading();
-    webApiConfig
-      .instance({
-        method: "post",
-        url: url,
-        data: params,
-        cancelToken:
-          (cancel &&
-            webApiConfig.instance.CancelToken(function executor(c) {
-              // executor 函数接收一个 cancel 函数作为参数
-              cancelFetch(cancel, interfaceKey).cancel = c;
-            })) ||
-          ""
-      })
+    customAxios({
+      method: "post",
+      url: url,
+      data: params,
+      cancelToken:
+        (!cancel && customAxios.CancelToken(function executor(c) {
+          // executor 函数接收一个 cancel 函数作为参数
+          cancelFetch(interfaceKey).cancel = c;
+        })) || ""
+    })
       .then(response => {
-        endLoading();
+        // endLoading();
         deleteInterceptors(interfaceKey); // 删除拦截器以及其实例
-        let _code = response.code;
-        /** 根据后台返回的状态码进行相应的处理 */
-        switch (_code) {
-          case 0:
-          case 1:
-          case 200:
-            break;
-          default:
-            console.log("示例代码都进行返回数据");
+        if (response.status === 200) {
+          return resolve(response.data);
+        } else {
+          /**
+           * 这里的数据处理请根据实际业务来操作
+           * 比如指定跳转到某页面
+           */
         }
       })
       .catch(error => {
